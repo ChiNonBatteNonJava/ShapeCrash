c0d219096d7bd7d3cbbb14020da1bbfecdab6781
Carlo Vespa <vespac@usi.ch>
2014-04-09 23:16:48 +0200

    First Commit
    Android Application Skeleton

---
+++ gen/com/chinonbattenonjava/saproject/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.chinonbattenonjava.saproject;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/chinonbattenonjava/saproject/MyGLSurfaceView.java
@@ -0,0 +1,13 @@
+package com.chinonbattenonjava.saproject;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+public class MyGLSurfaceView extends GLSurfaceView {
+
+	public MyGLSurfaceView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
